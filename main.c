27003f49d2a5f0d0f5255684859b35a601acf1e1
Duc111095 <huynhduc111095@gmail.com>
2024-10-11 16:43:07 +0700

    hello world

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int result[9][9];
-int markR[9][9];
-int markC[9][9];
-int markS[3][3][9];
+int markR[9][10];
+int markC[9][10];
+int markS[3][3][10];
 
 void solution() {
     for (int r = 0; r <= 8; r++) {
@@ -29,7 +29,7 @@ void find(int r, int c) {
             markR[r][v] = 1;
             markR[c][v] = 1;
             markS[r/3][c/3][v] = 1;
-            if (r == 8 && c == 8) {solution();}
+            if (r == 1 && c == 8) {solution();}
             else {
                 if (c == 8) {find(r + 1, 0);}
                 else {find(r, c + 1);}
