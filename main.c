e2e61356bba3625be9c63b43419da20d1b881eaa
Duc111095 <huynhduc111095@gmail.com>
2024-10-11 16:56:59 +0700

    message

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int result[9][9];
-int markR[9][10];
-int markC[9][10];
-int markS[3][3][10];
+int markR[9][9];
+int markC[9][9];
+int markS[3][3][9];
 
 void solution() {
     for (int r = 0; r <= 8; r++) {
@@ -16,9 +16,9 @@ void solution() {
 }
 
 int check(int r, int c, int v) {
-    if (markR[r][v] == 1) return 0;
-    if (markC[c][v] == 1) return 0;
-    if (markS[r/3][c/3][v] == 1) return 0;
+    if (markR[r][v-1] == 1) return 0;
+    if (markC[c][v-1] == 1) return 0;
+    if (markS[r/3][c/3][v-1] == 1) return 0;
     return 1;
 }
 
@@ -26,17 +26,17 @@ void find(int r, int c) {
     for (int v = 1; v <= 9 ; v++) {
         if (check(r, c, v) == 1) {
             result[r][c] = v;
-            markR[r][v] = 1;
-            markR[c][v] = 1;
-            markS[r/3][c/3][v] = 1;
-            if (r == 1 && c == 8) {solution();}
+            markR[r][v-1] = 1;
+            markR[c][v-1] = 1;
+            markS[r/3][c/3][v-1] = 1;
+            if (r == 0 && c == 8) {solution();}
             else {
                 if (c == 8) {find(r + 1, 0);}
                 else {find(r, c + 1);}
             }
-            markR[r][v] = 0;
-            markC[c][v] = 0;
-            markS[r/3][c/3][v] = 0;
+            markR[r][v-1] = 0;
+            markC[c][v-1] = 0;
+            markS[r/3][c/3][v-1] = 0;
         }
     }
 }
@@ -45,9 +45,9 @@ int main() {
     for (int r = 0; r <= 8; r ++) {
         for (int c = 0; c <= 8; c ++) {
             for (int v = 1; v <= 9; v ++) {
-                markR[r][v] = 0;
-                markC[c][v] = 0;
-                markS[r/3][c/3][v] = 0;
+                markR[r][v-1] = 0;
+                markC[c][v-1] = 0;
+                markS[r/3][c/3][v-1] = 0;
             }
         }
     }
